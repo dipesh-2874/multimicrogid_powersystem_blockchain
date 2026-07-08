a196f5940ca5a6851ad3706b93f6635cdd247e33
Cerebro-184 <mr.dipesh184@gmail.com>
2026-07-08 14:39:00 +0530

    acc add

--- scripts/mint.js
+++ scripts/mint.js
@@ -2,7 +2,13 @@ const { ethers } = require("hardhat");
 
 async function main() {
     const tokenAddress = "0x1F4C441Ff8bc81EccEA6c294D298326F93749950";
-    const recipient = ["0x541D16f0246Be1f926Ec162A5C7E98dF0089d43f", "0x828dfD39Ea825a432D5a841C36dab06c55c5049B", "0x13B4F80eCdEAeCa96214320B426420745CA9B4d9"];
+    const recipient = [
+        "0x541D16f0246Be1f926Ec162A5C7E98dF0089d43f", 
+        "0x828dfD39Ea825a432D5a841C36dab06c55c5049B", 
+        "0x13B4F80eCdEAeCa96214320B426420745CA9B4d9",
+        "0xfd6ef534067eB07fF3Eea6a21F870BAd1399203A",
+        "0x5bEA0b17A7D23e40C3cf6a75B3516315d933752c"
+    ];
     const amount = "10000";
 
     const EnergyToken = await ethers.getContractFactory("EnergyToken");
